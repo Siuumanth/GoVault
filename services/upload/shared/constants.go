766146db085f90425e743c991d2f20021f4d0502
Siuumanth <gsumanth3005@gmail.com>
2026-02-24 08:00:11 +0530

    add testing open api and incrtease upload proxy chunk size

--- services/upload/shared/constants.go
+++ services/upload/shared/constants.go
@@ -1,6 +1,6 @@
 package shared
 
-const ChunkSizeBytes int64 = 5 * 1024 * 1024 // 5 MB
+const ChunkSizeBytes int64 = 10 * 1024 * 1024 // 10 MB
 const UploadBasePath = "./sessions"
 
 const S3UsersURI = "s3://govault-files/users/"
