c99c9345f5e626d5c2b276ff3aaaed780396157a
Siuumanth <gsumanth3005@gmail.com>
2026-02-26 15:54:52 +0530

    fix connect db upload

--- services/upload/internal/database/connect.go
+++ services/upload/internal/database/connect.go
@@ -1,10 +1,17 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+// url is sent from main
 func Connect(url string) (*sql.DB, error) {
 	db, err := sql.Open("postgres", url)
 	if err != nil {
 		return nil, err
 	}
-
-	// --- ADD THESE LIMITS ---
 
 	// 1. The Waiting Room: Max physical connections
 	// Set this to ~80-90 if Postgres max_connections is 100
